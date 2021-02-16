30c1ffe32dbd464a880d04b1be1fd6e3df5122ed
Rahul Sharma <Rahul.Sharma2@coles.com.au>
2021-02-16 11:38:32 +1100

    Assessment Draft 1

---
+++ MetaDataSearch.ConsoleApp/MetaDataSearch.ConsoleApp/SearchCriteria.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MetaDataSearch.ConsoleApp
+{
+    public class SearchCriteria
+    {
+        public string PropertyName { get; set; }
+        public string PropertyType { get; set; }
+        public object SearchValue { get; set; }
+    }
+}
